92b30cfcda27064e4a18221a0fb04f92ad65e238
USER <tartilek@gmail.com>
2017-10-10 00:34:43 +0300

    Урок 1. Задание 2.

---
+++ src/Lesson1_Task2.java
@@ -0,0 +1,12 @@
+public class Lesson1_Task2 {
+    public static void main(String[] args) {
+
+        int n = 84;
+        int q;
+
+        q = n%10 + n/10;
+
+        System.out.println("Сумма цифр двухзначного числа n: " + q);
+
+    }
+}
