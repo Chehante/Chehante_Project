5ba0dcca3c13b6a8826cf8a2334a6b18a4492edb
USER <tartilek@gmail.com>
2017-10-10 00:24:19 +0300

    Урок 1. Задание 1.

---
+++ src/Lesson1_Task1.java
@@ -0,0 +1,10 @@
+public class Lesson1_Task1 {
+    public static void main(String[] args) {
+
+        int q = 21;
+        int w = 8;
+
+        System.out.println(q + " / " + w + " = " +  q/w + " и " + q%w + " в остатке");
+
+    }
+}
