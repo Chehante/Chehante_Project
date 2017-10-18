be85035919b776841285f7de0a5fe46bff98062b
trtilek <tartilek@gmail.com>
2017-10-18 14:00:21 +0300

    Third lesson

--- src/Lesson3/LessonMultidimensionalArrays.java
+++ src/Lesson3/LessonMultidimensionalArrays.java
@@ -7,10 +7,13 @@ public class LessonMultidimensionalArrays {
     public static void main(String[] args) {
 
         /////multidimensionalArrays_Task1_5columns_8raws/////
-        multidimensionalarraysTask1();
+        //multidimensionalarraysTask1();
+
+        /////multidimensionalArrays_Task2_8columns_5raws/////
+        //multidimensionalarraysTask2();
 
     }
-
+    /////multidimensionalArrays_Task1_5columns_8raws/////
     public static void multidimensionalarraysTask1(){
         Random rndm = new Random();
         int[][] multiarray = new int[8][5];
@@ -22,6 +25,22 @@ public class LessonMultidimensionalArrays {
             System.out.println("");
         }
         Arrays.toString(multiarray);
+    }
+
+    public static void multidimensionalarraysTask2(){
+        Random rndm = new Random();
+        int max = -99;
+        int[][] multiarray = new int[5][8];
+        for (int i = 0; i < multiarray.length; i++) {
+            for (int y = 0; y < multiarray[i].length; y++) {
+                multiarray[i][y] = -99 + rndm.nextInt(199);
+                System.out.print(multiarray[i][y]+ " ");
+                if (multiarray[i][y] > max)
+                    max = multiarray[i][y];
+            }
+            System.out.println("");
+        }
+        System.out.println("Наибольший элемент двумерного массива: " + max);
     }
 
 
