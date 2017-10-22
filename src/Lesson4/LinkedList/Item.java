f9c4bbcda54ff04de4558b6de8f3e037770bab4f
USER <tartilek@gmail.com>
2017-10-22 22:32:13 +0300

    Lesson 5 intList + linkedList

---
+++ src/Lesson4/LinkedList/Item.java
@@ -0,0 +1,11 @@
+package Lesson4.LinkedList;
+
+public class Item {
+
+    int number;
+    Item nextItem;
+
+    public Item(int a){
+        number = a;
+    }
+}
---
+++ src/Lesson5/LinkedList/Item.java
@@ -0,0 +1,11 @@
+package Lesson5.LinkedList;
+
+public class Item {
+
+    Object object;
+    Item nextItem;
+
+    public Item(Object a){
+        object = a;
+    }
+}
